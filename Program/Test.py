b9982d408ad4a2973079b0c183bd3a77dbaa0e69
Robert <rgagarner@gmail.com>
2017-06-17 12:57:47 +0100

    Test Commit 7

--- Program/Test.py
+++ Program/Test.py
@@ -11,5 +11,6 @@ def print_variable(v):
 print_something("How are you?")
 print_something("This is to test commit to Git")
 print_something("Test commit twice")
+print_something("Test branch")
 x = 5
 print_variable(x)
